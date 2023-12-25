5140ece5258fa2d50246024f03c18fd1d6572771
chen.tao <chentao0204@qq.com>
2023-12-25 10:03:44 +0800

    渲染帖子列表

---
+++ src/main/java/com/apress/demo/springblog/AppSpringBlog.java
@@ -0,0 +1,12 @@
+package com.apress.demo.springblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSpringBlog {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AppSpringBlog.class, args);
+  }
+}
