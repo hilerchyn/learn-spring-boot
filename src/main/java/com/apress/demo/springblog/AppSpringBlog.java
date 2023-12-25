406c30dc7defd98052c965c090dff98ace8c8243
chen.tao <chentao0204@qq.com>
2023-12-25 15:42:57 +0800

    打包 war

--- src/main/java/com/apress/demo/springblog/AppSpringBlog.java
+++ src/main/java/com/apress/demo/springblog/AppSpringBlog.java
@@ -2,10 +2,18 @@ package com.apress.demo.springblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AppSpringBlog {
-    public static void main(String[] args) {
+public class AppSpringBlog extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AppSpringBlog.class);
+    }
+
+    public static void main(String[] args){
         SpringApplication.run(AppSpringBlog.class, args);
     }
 }
