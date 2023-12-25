7efa5e3a99a7193019afc9bdf64c73683ad539ad
chen.tao <chentao0204@qq.com>
2023-12-25 16:24:36 +0800

    实现附件上传

--- src/main/java/com/apress/demo/springblog/controller/PostController.java
+++ src/main/java/com/apress/demo/springblog/controller/PostController.java
@@ -1,5 +1,8 @@
 package com.apress.demo.springblog.controller;
 
+import java.io.File;
+import java.nio.file.Files;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -7,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.apress.demo.springblog.domain.Post;
 import com.apress.demo.springblog.service.PostService;
@@ -43,5 +48,27 @@ public class PostController{
         postService.addPost(post);
 
         return "redirect:/posts";
+    }
+
+    @GetMapping("/fileUpload")
+    public String fileUpload(Model model) {
+        return "fileUpload";
+    }
+
+    @PostMapping("/uploadMyFile")
+    public String handleFileUpload(@RequestParam("myFile") MultipartFile file) {
+
+        if(!file.isEmpty()) {
+            String name = file.getOriginalFilename();
+
+            try {
+                byte[] bytes = file.getBytes();
+                Files.write(new File(name).toPath(), bytes);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return "redirect:/posts/fileUpload";
     }
 }
